265a8e5ba6ef54cd743ea8292e0233cb1a2b03f6
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2022-04-20 10:37:25 +0900

    add program

---
+++ app/src/main/java/com/example/janken/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.janken
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.speech.RecognizerIntent
+import android.view.View
+import androidx.core.content.edit
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.prefs.PreferenceChangeEvent
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gu.setOnClickListener{onJankenButtonTapped(it)}
+        choki.setOnClickListener{onJankenButtonTapped(it)}
+        pa.setOnClickListener{onJankenButtonTapped(it)}
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit{
+            clear()
+        }
+    }
+
+    fun onJankenButtonTapped(view: View?){
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("MY_HAND", view?.id)
+        startActivity(intent)
+    }
+}
